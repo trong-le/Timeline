4ae4776b61d571afac608d893b5fd91a00dc9efe
trong-le <trong.p.le.92@gmail.com>
2015-12-27 18:44:11 -0500

    Set up image grabs and collections of images

---
+++ ImageViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ImageViewController.swift
+//  Timeline
+//
+//  Created by Trong Le on 12/27/15.
+//  Copyright © 2015 Trong Le. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController
+{
+    
+    // get image URL making sure it's not nil
+    var imageURL: NSURL? {
+        didSet {
+            image = nil
+            if view.window != nil {
+                getImage()
+            }
+        }
+    }
+    
+    private func getImage() {
+        if let url = imageURL {
+            let imageData = NSData(contentsOfURL: url)
+            if imageData != nil {
+                image = UIImage(data: imageData!)
+            } else {
+                image = nil
+            }
+        }
+    }
+    
+    private var imageView = UIImageView()
+    
+    private var image: UIImage? {
+        get { return imageView.image }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(imageView)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if image == nil {
+            getImage()
+        }
+    }
+
+}
---
+++ Timeline/images.swift
@@ -0,0 +1,19 @@
+//
+//  images.swift
+//  Timeline
+//
+//  Created by Trong Le on 12/27/15.
+//  Copyright © 2015 Trong Le. All rights reserved.
+//
+
+import Foundation
+
+struct LocationImages {
+    static let Seattle = NSURL(string: "http://media-cdn.tripadvisor.com/media/photo-s/03/9b/30/26/seattle.jpg")
+    struct Ohio{
+        static let Hillsboro = NSURL(string: "http://pix.epodunk.com/OH/oh_hillsboro01.jpg")
+        static let Columbus = NSURL(string: "http://wilsonresourcesinc.com/wp-content/uploads/2013/04/columbus.jpg")
+        static let Cincinnati = NSURL(string: "http://www.proalertsecuritysystems.com/images/cincinnati2.jpg")
+    }
+
+}
